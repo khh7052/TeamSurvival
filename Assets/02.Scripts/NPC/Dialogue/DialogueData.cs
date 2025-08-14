a8a772a2a0299565681720061981e4012eb379e8
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-08-14 15:16:42 +0900

    [Refactor] DialogueData 상속 변경

--- Assets/02.Scripts/NPC/Dialogue/DialogueData.cs
+++ Assets/02.Scripts/NPC/Dialogue/DialogueData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "DialogueData", menuName = "Data/DialogueData")]
-public class DialogueData : ScriptableObject
+public class DialogueData : BaseScriptableObject
 {
     public DialogueLine[] lines; // 대사 목록
 
