ae50f221ffb32c4e136a98ad0c3993e082a6b8da
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-08-14 18:35:55 +0900

    [Feat] NPC 대화 타이핑 효과 추가

--- Assets/02.Scripts/NPC/Dialogue/DialogueLine.cs
+++ Assets/02.Scripts/NPC/Dialogue/DialogueLine.cs
@@ -8,4 +8,6 @@ public class DialogueLine
     public string speaker; // 화자
     [TextArea]
     public string text; // 대사 내용
+    public bool useTypingEffect = true; // 타이핑 효과 사용 여부
+    public float typingSpeed = 0.1f; // 타이핑 속도 (초 단위)
 }
