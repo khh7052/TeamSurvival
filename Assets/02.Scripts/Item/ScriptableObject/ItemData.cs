7f12d4b5083635c8b3f4a1de0f589677a16cf2a7
SJW1737 <sic470212@naver.com>
2025-08-14 20:06:41 +0900

    [feat] 아이템 데이터 스크립트에 아이템 타입 무기,도구 구분

--- Assets/02.Scripts/Item/ScriptableObject/ItemData.cs
+++ Assets/02.Scripts/Item/ScriptableObject/ItemData.cs
@@ -2,41 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public enum ItemType
-    {
-        Resource, //자원
-        Equipable, //장착
-        Consumable, //소모품
-    }
+public enum ItemType
+{
+    Resource, //자원
+    Equipable, //장착
+    Consumable, //소모품
+}
 
-    public enum ConsumableType
-    {
-        Hunger, //배고픔
-        Thirst, //갈증
-        Health, //체력
-        Stamina, //내구력
-    }
+public enum ConsumableType
+{
+    Hunger, //배고픔
+    Thirst, //갈증
+    Health, //체력
+    Stamina, //내구력
+}
 
-    [System.Serializable]
-    public class ItemDataConsumable
-    {
-        public ConsumableType type;
-        public float value;
-    }
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
 
-    [CreateAssetMenu(fileName = "Item", menuName = "New Item")]
-    public class ItemData : BaseScriptableObject
-    {
-        [Header("Info")]
-        public string description; //설명
-        public ItemType type;
-        public Sprite icon;
-        public GameObject dropPrefab; //보이는 프리팹
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : BaseScriptableObject
+{
+    [Header("Info")]
+    public string description; //설명
+    public ItemType type;
+    public Sprite icon;
+    public GameObject dropPrefab; //보이는 프리팹
 
-        [Header("Stacking")] 
-        public bool canStack; //아이템 한칸에 중복 여부
-        public int maxStackAmount; //중복 개수 제한
+    [Header("Stacking")] 
+    public bool canStack; //아이템 한칸에 중복 여부
+    public int maxStackAmount; //중복 개수 제한
 
-        [Header("Consumable")]
-        public ItemDataConsumable[] consumables; //소모품 효과
-    }
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables; //소모품 효과
+
+    [Header("Equip Usage")]
+    public bool isWeapon;   //무기 용도
+    public bool isTool;     //도구 용도
+
+    [Header("Weapon")]
+    public int weaponDamage = 10;           //무기 공격력
+    public float weaponAttackDelay = 1f;    //무기 공격 딜레이
+    public float weaponAttackDistance = 1f; //무기 사거리
+
+    [Header("Tool")]
+    public float toolGatherPower = 5f;      //자원 채취 힘
+    public float toolDistance = 1f;         //도구 사거리
+}
