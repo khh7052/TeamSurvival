f38e1b34588805cef44036ccd0129242eb76eff7
Kim Jun Hyeong <kheami35@gmail.com>
2025-08-16 13:49:59 +0900

    [ Feat ] ResourceSpawner
    기능 베타

--- Assets/02.Scripts/Map/ResourceSpawner.cs
+++ Assets/02.Scripts/Map/ResourceSpawner.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class ResourceSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Spawn Setting")]
+    public int resourceID;
+    public float spawnInterval = 3f;
+    public Vector3 spawnAreaMin;
+    public Vector3 spawnAreaMax;
+
+    private bool canSpawn = false;
+
+    private void Start()
     {
-        
+        StartCoroutine(WaitForFactorySpawn());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator WaitForFactorySpawn()
     {
-        
+        while(!Factory.Instance.IsInitialized || !ObjectPoolingManager.Instance.IsInitialized)
+        {
+            yield return null;
+        }
+
+        canSpawn = true;
+
+        while(canSpawn)
+        {
+            SpawnResource();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    void SpawnResource()
+    {
+        Vector3 spawnPos = new Vector3(
+            Random.Range(spawnAreaMin.x, spawnAreaMax.x),
+            Random.Range(spawnAreaMin.y, spawnAreaMax.y),
+            Random.Range(spawnAreaMin.z, spawnAreaMax.z)
+            );
+
+        GameObject resource = Factory.Instance.CreateByID<ItemData>(resourceID);
+        if(resource != null)
+        {
+            resource.transform.position = spawnPos;
+        }  
     }
 }
