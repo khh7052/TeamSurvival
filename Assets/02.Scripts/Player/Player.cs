60f7af5ea074c6895c0e62d0d659c7853ed79f37
Kim Jun Hyeong <kheami35@gmail.com>
2025-08-21 11:55:29 +0900

    [ feat ] Player Die
    테스트용..

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour, IDeathBehavior
 {
@@ -31,8 +32,34 @@ public class Player : MonoBehaviour, IDeathBehavior
         GameManager.player = this;
     }
 
+    private void Update()
+    {
+        Die();
+    }
+
     public void Die()
     {
+        if (model.isDie) return;
 
+        if (model.health.CurValue <= 0f)
+        {
+            model.isDie = true;
+
+            Debug.Log("ав╬З╣Ш");
+            Animator animator = GetComponent<Animator>();
+            if (animator != null) animator.enabled = false;
+
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            if (agent != null) agent.enabled = false;
+
+            controller.canLook = false;
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.constraints = RigidbodyConstraints.None;
+                rb.AddForce(transform.right * 1.5f, ForceMode.Impulse); 
+                rb.AddTorque(Vector3.back * 1.5f, ForceMode.Impulse);  
+            }
+        }
     }
 }
