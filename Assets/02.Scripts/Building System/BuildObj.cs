5a03d8741507db7582227760535ffb9937261230
C0mm4 <comonhuman@gmail.com>
2025-08-21 12:42:05 +0900

    Feat : 데미지 입을 때 화면 빨갛게, 메쉬렌더러 빨갛게

--- Assets/02.Scripts/Building System/BuildObj.cs
+++ Assets/02.Scripts/Building System/BuildObj.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -13,13 +14,17 @@ public class BuildObj : MonoBehaviour, IDamageable
 
     public bool IsDead => Health.CurValue <= 0;
 
+    public Action OnHitEvent { get; set; }
+
+    public MeshRenderer mesh;
+    private Coroutine coroutine;
 
     public void Initialize()
     {
         animationHandler = GetComponent<AnimationHandler>();
         Health.OnChanged -= Health_OnChanged;
         Health.Init();
-
+        OnHitEvent += OnHit;
         Health.OnChanged += Health_OnChanged;
 
     }
@@ -27,6 +32,7 @@ public class BuildObj : MonoBehaviour, IDamageable
     public void TakePhysicalDamage(int damage)
     {
         Health.Subtract(damage);
+        OnHitEvent?.Invoke();
     }
 
     private void Health_OnChanged()
@@ -47,5 +53,35 @@ public class BuildObj : MonoBehaviour, IDamageable
     private void OnDisable()
     {
         BuildingManager.Instance?.UnregisterBuild(key);
+        OnHitEvent -= OnHit;
+    }
+
+    public void OnHit()
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+        coroutine = StartCoroutine(SetMeshColorAtDamage());
+    }
+
+    private IEnumerator SetMeshColorAtDamage()
+    {
+        Color startColor = Color.red;       // 시작 색 (빨강)
+        Color endColor = Color.white;       // 최종 색 (하양)
+        float duration = 1.0f;              // 효과 지속 시간 (1초)
+        float elapsed = 0f;
+
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration; // 0 → 1 로 증가
+            mesh.material.color = Color.Lerp(startColor, endColor, t);
+            yield return null;
+        }
+
+        // 보정: 최종적으로 흰색 확정
+        mesh.material.color = endColor;
     }
 }
